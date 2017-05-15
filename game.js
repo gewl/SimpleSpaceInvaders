545de6c8dbe628d0229a1ba51ad52fc62c32a359
gil lawson <gewl@users.noreply.github.com>
2017-05-14 21:31:47 -0400

    tidy board, enemies moving-need player movement

--- game.js
+++ game.js
@@ -5,6 +5,9 @@ function Game() {
 	this.boardWidth = 480;
 	this.cellWidth = this.boardWidth/16;
 
+	this.intervalId = null;
+	this.travelingLeft = true;
+
 	// object positioins	
 	this.cells = [
 		[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -37,41 +40,112 @@ Game.prototype.initialize = function() {
 
 	document.body.appendChild(canvas);
 
-	for (let i = 0; i <= this.boardWidth; i += this.cellWidth) {
-		this.ctx.beginPath();
-		this.ctx.moveTo(0, i);
-		this.ctx.lineTo(this.boardWidth, i);
-		this.ctx.lineWidth = 2;
-		this.ctx.stroke();
+	this.draw()
+}
+
+Game.prototype.start = function() {
+	var game = this;
+	this.intervalId = setInterval(function() {
+		game.loop()
+	}, 200)
+}
+
+Game.prototype.loop = function() {
+	var width = this.cells.length
+	if (this.travelingLeft) {
+		var atLeftEdge = false;
+
+		for (var i = 0; i < width; i++) {
+			if (this.cells[i][0] === "E") {
+				atLeftEdge = true;
+				break;
+			}
+		}
+
+		if (atLeftEdge) {
+			this.travelingLeft = false;
+		} else {
+			for (var y = 0; y < width; y++) {
+				var row = this.cells[y];
+				for (var x = 0; x < width; x++) {
+					var cell = row[x];
+					if (cell === "E") {
+						row[x-1] = "E";
+						row[x] = 0;
+					}
+				}
+			}
+		}
+	} else {
+		var atRightEdge = false;
+
+		for (var i = 0; i < width; i++) {
+			if (this.cells[i][width-1] === "E") {
+				atRightEdge = true;
+				break;
+			}
+		}
+
+		if (atRightEdge) {
+			this.travelingLeft = true;
+		} else {
+			for (var y = width - 1; y >= 0; y--) {
+				var row = this.cells[y];
+				for (var x = width - 1; x >= 0; x--) {
+					var cell = row[x];
+					if (cell === "E") {
+						row[x+1] = "E";
+						row[x] = 0;
+					}
+				}
+			}
+		}
 	}
 
-	this.draw()
+	this.draw();
 }
 
 // draw board onto canvas
 Game.prototype.draw = function() {
-	var length = this.cells.length;
-	for (var y = 0; y < length; y++) {
+	var width = this.cells.length;
+	for (var y = 0; y < width; y++) {
 		var row = this.cells[y];
-		for (var x = 0; x < length; x++) {
+		for (var x = 0; x < width; x++) {
 			var cell = row[x];
-			if (cell != 0) {
-				this.drawCell(cell, x, y);
-			}
+			this.drawCell(cell, x, y);
 		}
+	}
+	for (let i = 0; i <= this.boardWidth; i += this.cellWidth) {
+		this.ctx.beginPath();
+		this.ctx.moveTo(0, i);
+		this.ctx.lineTo(this.boardWidth, i);
+		this.ctx.moveTo(i, 0);
+		this.ctx.lineTo(i, this.boardWidth);
+		this.ctx.lineWidth = 1;
+		this.ctx.stroke();
 	}
 }
 
 Game.prototype.drawCell = function(cell, x, y) {
+	var cellWidth = this.cellWidth, ctx = this.ctx;
 	// choose color to draw based on vallue of cell
 	switch (cell) {
 		case 'E':
-			this.ctx.fillStyle = "red";
+			ctx.fillStyle = "red";
 			break;
 		case 'P':
-			this.ctx.fillStyle = "blue";
+			ctx.fillStyle = "blue";
+			break;
+		default:
+			ctx.fillStyle = "white";
 			break;
 	}
+
+	// ctx.beginPath();
+	// ctx.arc(x + cellWidth/2, y + cellWidth/2, cellWidth/2 - 3, 0, Math.PI*2, true);
+	// ctx.lineWidth = 1;
+	// ctx.fill();
+	// ctx.stroke();
 
 	this.ctx.fillRect(x * this.cellWidth, y * this.cellWidth, this.cellWidth, this.cellWidth);
 
@@ -79,3 +153,4 @@ Game.prototype.drawCell = function(cell, x, y) {
 
 var game = new Game();
 game.initialize();
+game.start();
